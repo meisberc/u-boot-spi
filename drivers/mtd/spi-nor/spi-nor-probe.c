323af583f1273704a948144543d8fa1e24eb60e6
Jagan Teki <jteki@openedev.com>
2016-02-14 02:38:45 +0530

    mtd: spi-nor: Add spi_flash_probe for mtd-dm-spi-nor
    While probing spi-nor in SPL spi_flash_probe is needed,
    so add the flash probe code in spi-nor-probe.c
    
    Cc: Simon Glass <sjg@chromium.org>
    Cc: Bin Meng <bmeng.cn@gmail.com>
    Cc: Mugunthan V N <mugunthanvnm@ti.com>
    Cc: Michal Simek <michal.simek@xilinx.com>
    Cc: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Signed-off-by: Jagan Teki <jteki@openedev.com>

--- drivers/mtd/spi-nor/spi-nor-probe.c
+++ drivers/mtd/spi-nor/spi-nor-probe.c
@@ -9,6 +9,21 @@
 #include <spi.h>
 #include <spi_flash.h>
 
+/*
+ * TODO(sjg@chromium.org): This is an old-style function. We should remove
+ * it when all SPI flash drivers use dm
+ */
+spi_flash_t *spi_flash_probe(unsigned int bus, unsigned int cs,
+			     unsigned int max_hz, unsigned int spi_mode)
+{
+	struct udevice *dev;
+
+	if (spi_flash_probe_bus_cs(bus, cs, max_hz, spi_mode, &dev))
+		return NULL;
+
+	return dev_get_uclass_priv(dev);
+}
+
 int spi_flash_probe_bus_cs(unsigned int busnum, unsigned int cs,
 			   unsigned int max_hz, unsigned int spi_mode,
 			   struct udevice **devp)
